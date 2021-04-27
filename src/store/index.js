2cbcc74eec7cfbfcd3a3df34df925fbd42983ded
jmanuellh <manuellh2008@gmail.com>
2021-04-27 02:24:36 -0500

    opciones para agregar y obtener elementos

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,23 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    productos: [
+      {
+        nombre: ''
+      }
+    ]
+  },
+  getters: {
+    obtenerProductos: state => state.productos
   },
   mutations: {
+    AGREGAR_PRODUCTO: (state, producto) => state.productos.push(producto)
   },
   actions: {
+    agregarProducto: ({commit}, producto) => {
+      console.log("entro a Acction agregarProducto")
+      commit('AGREGAR_PRODUCTO', producto)
+    }
   },
   modules: {
   }
