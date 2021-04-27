28f8765cbeb601e81397650fef1965774614d2c7
jmanuellh <manuellh2008@gmail.com>
2021-04-27 02:27:09 -0500

    opciones para tabla de elementos con vuex

---
+++ src/components/TablaProductos.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <table>
+      <thead>
+        <th>Nombre</th>
+      </thead>
+      <tbody
+        v-for="producto in obtenerProductos"
+        :key="producto.nombre"
+      >
+        <td>
+          {{ producto.nombre }}
+        </td>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  computed: {
+    ...mapGetters([
+      'obtenerProductos'
+    ])
+  },
+}
+</script>
