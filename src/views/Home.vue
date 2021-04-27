02c184cfa7955d8d94cf08fcf2d22ea70b8bf2cc
jmanuellh <manuellh2008@gmail.com>
2021-04-27 02:26:17 -0500

    quitado uso de componente default de vue cli y agregados componentes agregar y tabla

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <agregar-producto />
+    <tabla-productos />
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import TablaProductos from "@/components/TablaProductos.vue"
+import AgregarProducto from "@/components/AgregarProducto.vue"
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HelloWorld,
+    TablaProductos,
+    AgregarProducto
   }
 }
 </script>
+
+    AgregarProducto
