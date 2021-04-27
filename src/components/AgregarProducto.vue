5a35e4d0e8a4b10d0ff947082f4645a285d2f65e
jmanuellh <manuellh2008@gmail.com>
2021-04-27 02:26:53 -0500

    opciones para agregar elemento con vuex

---
+++ src/components/AgregarProducto.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <div>
+      <input type="text" v-model="nuevoProducto.nombre">
+    </div>
+    <div>
+      <button @click="agregarProducto(Object.assign({}, nuevoProducto))">Agregar</button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapActions } from "vuex";
+
+export default {
+  data() {
+    return {
+      nuevoProducto: {
+        nombre: ''
+      }
+    }
+  },
+  methods: {
+    ...mapActions([
+      'agregarProducto'
+    ])
+  }
+}
+</script>
